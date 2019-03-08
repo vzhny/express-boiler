21473f7575756a852ca8604f2257f21754cb36f1
Diego Vizhnay <diego@vzhny.com>
2019-03-08 16:56:14 -0500

    🎉 Initial databaseSetup.js | 03/08

---
+++ src/tests/databaseSetup.js
@@ -0,0 +1,16 @@
+import database from '../db/database';
+
+export const dropAllTables = () => {
+  database
+    .query('TRUNCATE users;')
+    .then(result => console.log("'users' tables truncated."))
+    .catch(error => console.log("An error occurred truncating the 'users' table", error));
+  // Add any more tables that need to be cleared below
+};
+
+export const closeDatabaseConnection = () => {
+  database
+    .end()
+    .then(() => process.exit(0))
+    .catch(error => console.log(error));
+};
