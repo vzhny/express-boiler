bb1df0944a286232bc2f331494b69ed9eb3d9900
Diego Vizhnay <diego@vzhny.com>
2019-03-08 16:49:56 -0500

    🎉 Initial babel.config.js | 03/08

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        browsers: ['>0.25%', 'not ie 11', 'not op_mini all'],
+      },
+      useBuiltIns: 'usage',
+    },
+  ],
+];
+
+const plugins = ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties'];
+
+module.exports = { presets, plugins };
