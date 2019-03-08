070a492930549c2767400e1b31de4f5fcc85a625
Diego Vizhnay <diego@vzhny.com>
2019-03-08 16:56:29 -0500

    🎉 Initial index.test.js | 03/08

---
+++ src/tests/index.test.js
@@ -0,0 +1,7 @@
+import serverTests from './server.test';
+import userTests from './users.test';
+
+/* eslint-disable jest/valid-describe */
+
+describe('Server Tests', serverTests);
+describe('User Tests', userTests);
