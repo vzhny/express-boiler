eccab24790f002076d1607c4682291acd237a542
Diego Vizhnay <diego@vzhny.com>
2019-03-12 15:52:28 -0400

    📝 Added beforeAll and afterAll functions as global hooks for all ran tests | 03/12

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,7 +1,11 @@
 import serverTests from '@/tests/server.test';
 import userTests from '@/tests/users.test';
+import { rollbackAndMigrate, closeDatabaseConnection } from '@/tests/databaseSetup';
 
 /* eslint-disable jest/valid-describe */
+beforeAll(() => rollbackAndMigrate());
+
+afterAll(() => closeDatabaseConnection());
 
 describe('Server Tests', serverTests);
 describe('User Tests', userTests);
