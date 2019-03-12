7109e837042cb2f1d01b13c76a5ede54dce054e3
Diego Vizhnay <diego@vzhny.com>
2019-03-12 18:39:43 -0400

    🛠 Refactored the config file to set env variables using a switch block instead of through if statements | 03/12

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,17 @@ import {} from 'dotenv/config';
 
 const env = process.env.NODE_ENV;
 
-if (env === 'development') {
-  process.env.PG_CONNECT_STRING = process.env.PG_CONNECT_STRING_DEV;
-
-  process.env.JWT_SECRET = process.env.JWT_SECRET_DEV;
-  process.env.PG_DB_NAME = process.env.PG_DB_NAME_DEV;
-} else if (env === 'test') {
-  process.env.PG_CONNECT_STRING = process.env.PG_CONNECT_STRING_TEST;
-
-  process.env.JWT_SECRET = process.env.JWT_SECRET_TEST;
-  process.env.PG_DB_NAME = process.env.PG_DB_NAME_TEST;
+switch (env) {
+  case 'development':
+    process.env.PG_CONNECT_STRING = process.env.PG_CONNECT_STRING_DEV;
+    process.env.JWT_SECRET = process.env.JWT_SECRET_DEV;
+    process.env.PG_DB_NAME = process.env.PG_DB_NAME_DEV;
+    break;
+  case 'test':
+    process.env.PG_CONNECT_STRING = process.env.PG_CONNECT_STRING_TEST;
+    process.env.JWT_SECRET = process.env.JWT_SECRET_TEST;
+    process.env.PG_DB_NAME = process.env.PG_DB_NAME_TEST;
+    break;
+  default:
+    break;
 }
