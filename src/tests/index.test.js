4ad6d585097a2ec6653d3d756bcfd7e8880db47d
Diego Vizhnay <diego@vzhny.com>
2019-03-15 10:58:58 -0400

    🛠 Updated imported functions from db/database.js | 03/15

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,11 +1,11 @@
 import serverTests from '@/tests/server.test';
 import userTests from '@/tests/users.test';
-import { rollbackAndMigrate, closeDatabaseConnection } from '@/tests/databaseSetup';
+import { rollbackAndMigrate, disconnectFromDatabase } from '@/db/database';
 
 /* eslint-disable jest/valid-describe */
 beforeAll(() => rollbackAndMigrate());
 
-afterAll(() => closeDatabaseConnection());
+afterAll(() => disconnectFromDatabase());
 
 describe('Server Tests', serverTests);
 describe('User Tests', userTests);
