2d2acfec168183de7ed135ebfa9c87a214f4260c
Diego Vizhnay <diego@vzhny.com>
2019-03-16 18:45:45 -0400

    🛠 Removed if block checking for non-test env | 03/16

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 import app from '@/app';
 
-const env = process.env.NODE_ENV;
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-  if (env !== 'test') {
-    console.log(`Server listening on port ${port}.`);
-  }
+  console.log(`Server listening on port ${port}.`);
 });
 
 export default server;
